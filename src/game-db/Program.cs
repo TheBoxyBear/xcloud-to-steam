4769cc1ec30d8c6d2bdd10dded6d7cd7484856d6
TheBoxyBear <15822255+TheBoxyBear@users.noreply.github.com>
2026-03-03 14:54:59 -0500

    Add game-db script to generate markdown table

---
+++ src/game-db/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+using xCloudToSteam.Core;
+using xCloudToSteam.xCloud;
+using xCloudToSteam.xCloud.Model;
+
+IEnumerable<ProductDetails> games = xCloudShortcutManager.FilterEditions(
+		xCloudApi.GetDetails(
+			xCloudApi.GetCatalog().ToBlockingEnumerable(), "CA").ToBlockingEnumerable());
+
+StringBuilder sb = new(
+"""
+# xCloud Game Database
+
+Use this reference to create game shortcuts in your Steam library. Use the *Store ID* as the launch option.
+
+| Title | Publisher | Store ID | xCloud ID |
+|-------|-----------|------------------|----------|
+
+""");
+
+foreach (ProductDetails details in games.OrderBy(details => details.ProductTitle))
+	sb.AppendLine($"| {details.ProductTitle.Replace("©", "").Replace("®", "").Replace("™", "")} | {details.PublisherName} | {details.StoreId} | {details.XCloudTitleId} |");
+
+File.WriteAllText("output.md", sb.ToString());
